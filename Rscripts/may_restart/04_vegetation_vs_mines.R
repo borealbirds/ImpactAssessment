38a083ccd2ae7e2251ed1ee459555f18347cd449
mannfred boehm <mannfred.boehm@ubc.ca>
2025-07-09 22:37:50 -0700

    create mine impact summary

--- Rscripts/may_restart/04_vegetation_vs_mines.R
+++ Rscripts/may_restart/04_vegetation_vs_mines.R
@@ -3,10 +3,21 @@
 # author: Mannfred Boehm
 # created: June 11, 2025
 # ---
+
+
+#1. attach packages----
 library(furrr)
 library(progress)
 library(terra)
 library(tidyverse)
+
+# set up parallel computing
+future::plan(multisession, workers = parallel::detectCores() - 2)
+options(future.globals.maxSize = 4 * 1024^3)
+
+
+
+#2. import and index covariate data----
 
 root <- "G:/Shared drives/BAM_NationalModels5"
 
@@ -31,11 +42,13 @@ mine_years <-
   list.files(file.path(root, "gis", "other_landscape_covariates"), pattern = "nobuffer_masked", full.names = TRUE)
 
 
+
+#3. define mine impacts function----
 # FUNCTION:
 # import raster stack for BCR i x Year j
 # import mining data for Year j and crop to BCR i
-# for every mine x covariate tuple, calculate pixel values in the 9x9 square surrounding the
-# mine center and enter 9 rows into a dataframe with columns:
+# for every mine x covariate tuple, calculate pixel values in a  n x n grid surrounding the
+# mine center and enter n^2 rows into a dataframe with columns:
 # year, bcr, var, mine, pixel_distance,  value 
 # enter the dataframe as an element in a list of 133 (number of Canadian BCR stacks excluding 1985)
 
@@ -86,7 +99,7 @@ estimate_mine_impact <- function(covariate_stack_path){
     r_range <- (row - 10):(row + 10)
     c_range <- (col - 10):(col + 10)
     
-    grid_coords_k <- expand.grid(row = r_range, col = c_range)
+    grid_coords_k <- distinct(expand.grid(row = r_range, col = c_range))
   
     # extract values for covariate m from the grid surrounding mine k
     purrr::map_dfr(vars, function(var_m) {
@@ -98,7 +111,7 @@ estimate_mine_impact <- function(covariate_stack_path){
       # isolate one covariate layer (m) from raster stack ij (BCR x year)
       cov_m <- stack_ij[[var_m]]
       
-      # index the cells from the grid surrounding grid
+      # index the cells from the surrounding grid
       cells_k <- terra::cellFromRowCol(cov_m, row = grid_coords_k$row, col = grid_coords_k$col)
       
             # find the pixel values from the grid surrounding mine k
@@ -119,7 +132,7 @@ estimate_mine_impact <- function(covariate_stack_path){
         dplyr::mutate(pixel_distance = sqrt((x - center_xy[1])^2 + (y - center_xy[2])^2))
       
       # index distance vs covariate values along with current BCR x year x mine info
-      # Combine with covariate values
+      # combine with covariate values
       tibble(
         year = year,
         bcr = bcr,
@@ -127,7 +140,9 @@ estimate_mine_impact <- function(covariate_stack_path){
         mine_id_x = center_xy[1],
         mine_id_y = center_xy[2],
         pixel_distance = dist_vals$pixel_distance,
-        value = values_m[[var_m]])
+        value = values_m[[var_m]]) |> 
+      distinct()
+      
     }) # finish iterating over all covariates for the current mine
         
   }) # finish iterating over all mines in the current BCR x year
@@ -137,19 +152,168 @@ estimate_mine_impact <- function(covariate_stack_path){
 } # close function
 
 
-# set up parallel computing
-future::plan(multisession, workers = parallel::detectCores() - 2)
-options(future.globals.maxSize = 4 * 1024^3)
-
-
 # wrap `estimate_mine_impact` function to return a value instead of an error
 safe_estimate_mine_impact <- purrr::possibly(estimate_mine_impact, otherwise = NULL)
 
 # run mine impact estimation in parallel
 mine_impacts_list <- furrr::future_map(.x = stacks$file_name, .f = safe_estimate_mine_impact, .progress = TRUE)
-saveRDS(mine_impacts_list, file="C:/Users/mannf/Downloads/mine_impacts_list.rds")
+saveRDS(mine_impacts_list, file=file.path("C:/Users/mannf/Downloads/mine_impacts_list.rds"))
 
-mine_impacts_df <- bind_rows(mine_impacts_list)
 
+
+
+#4. convert mine impacts list (every element is a bcr x year) into a dataframe----
+mine_impacts_df <- 
+  mine_impacts_list |> 
+  dplyr::bind_rows() |> 
+  tidyr::drop_na() |> 
+  dplyr::group_by(var) |> 
+  dplyr::mutate(value_scaled = (value - min(value, na.rm = TRUE)) /
+                  (max(value, na.rm = TRUE) - min(value, na.rm = TRUE))) |> 
+  dplyr::ungroup() 
+
+
+mine_impacts_df_cont <- 
+  dplyr::filter(mine_impacts_df, !(var %in% c("SCANFI_1km", "VLCE_1km", "MODISLCC_1km", "MODISLCC_5x5"))) |> 
+  tidyr::drop_na()
+# saveRDS(mine_impacts_df_cont, file="C:/Users/mannf/Downloads/mine_impacts_df_cont.rds")
+mine_impacts_df_cont <- readRDS(file="C:/Users/mannf/Downloads/mine_impacts_df_cont.rds")
+
+
+mine_impacts_df_cat <- 
+  dplyr::filter(mine_impacts_df, var %in% c("SCANFI_1km", "VLCE_1km", "MODISLCC_1km", "MODISLCC_5x5")) |> 
+  tidyr::drop_na()
+# saveRDS(mine_impacts_df_cat, file="C:/Users/mannf/Downloads/mine_impacts_df_cat.rds")
+mine_impacts_df_cat <- readRDS(file="C:/Users/mannf/Downloads/mine_impacts_df_cat.rds")
+
+
+
+#5. quantify mine impacts by regression---- 
+
+# estimate slope of distance from mine vs covariate value
+# do this for every mine and every year (do not average across years)
+quantify_impact_by_mine <- function(impact_data) {
+  
+  # store the var name for the current covariate data unit 
+  # (note: `group_split` is used to run this function in parallel over each covariate)
+  var_name <- unique(impact_data$var)   
+  
+  impact_summary <-
+    impact_data |> 
+    dplyr::group_by(year, mine_id_x, mine_id_y) |> 
+    dplyr::group_modify(~{  # define a function that can iterate on the grouped tibble
+      
+      df <- .x
+      df <- filter(df, !is.na(value_scaled), !is.na(pixel_distance))
+      
+      if (nrow(df) < 3 || length(unique(df$pixel_distance)) == 1) {
+        return(tibble(beta = NA, p_value = NA, adj_r2 = NA))
+      }
+      
+      model <- lm(value_scaled ~ pixel_distance, data = df)
+      model_summary <- summary(model)
+      tibble(beta = coef(model_summary)["pixel_distance", "Estimate"],
+             p_value = coef(model_summary)["pixel_distance", "Pr(>|t|)"],
+             adj_r2  = model_summary$adj.r.squared)
+      
+      }) |> 
+    dplyr::ungroup() |> 
+    dplyr::group_by(mine_id_x, mine_id_y) |> 
+    dplyr::summarise(
+      beta_mean = mean(beta, na.rm = TRUE),
+      beta_sd = sd(beta, na.rm = TRUE),
+      p_mean = mean(p_value, na.rm = TRUE),
+      p_sd = sd(p_value, na.rm = TRUE),
+      adj_r2_mean = mean(adj_r2, na.rm = TRUE),
+      adj_r2_sd = sd(adj_r2, na.rm =TRUE),
+      .groups = "drop"
+    ) |> 
+    dplyr::mutate(var = var_name) 
+  
+  return(impact_summary)
+}
+
+# split dataset by number of covariates (e.g., 39 vars -> 39 units to compute through)
+mine_impacts_split <- dplyr::group_split(mine_impacts_df_cont, var)
+
+# fit lm to impact vs pixel distance 
+# compute in parallel for every covariate 
+mine_impact_summary <- 
+  furrr::future_map(.x = mine_impacts_split, .f = quantify_impact_by_mine, .progress = TRUE) |> 
+  dplyr::bind_rows() |> 
+  dplyr::arrange(desc(abs(beta_mean)))
+
+write_csv(x = mine_impact_summary, file = file.path(getwd(), "/data/derived_data/mine_impact_summary.csv"))
+
+
+# inspect mines of interest by converting coordinates to EPSG:4326
+xy_proj <- vect(matrix(c(-2182500,	3997500), ncol = 2), crs = "EPSG:5070")
+
+# Reproject to decimal degrees (lat/lon, EPSG:4326)
+xy_lonlat <- project(xy_proj, "EPSG:4326")
+
+# See the coordinates
+crds(xy_lonlat)
+
+
+
+#6. plot impact vs distance from mine (for selected mines)----
+
+
+# filter for some specific mine x year combination 
+plot_data <- dplyr::filter(mine_impacts_df_cont, 
+                           dplyr::near(mine_id_x, -1754500) & 
+                           dplyr::near(mine_id_y, 3151500) & 
+                           var == "SCANFILodgepolePine_1km")
+  
+  
+# exported at 1000 x 650
+ggplot(plot_data, aes(x = pixel_distance, y = value_scaled, color = factor(year), group = year)) +
+  geom_smooth(se = FALSE, method = "loess", formula = y ~ x, span = 0.75, alpha = 0.1) +
+  geom_point(alpha = 0.1, size = 2) + 
+  #facet_wrap(~var) +
+  labs(
+    x = "distance from mine (m)",
+    y = "scaled covariate value",
+    color = "year",
+    title = paste0("mine impacts on ", plot_data$var[1], " for mine ", 
+                   plot_data$mine_id_x[1], "-", plot_data$mine_id_y[1])
+  ) +
+  scale_color_viridis_d(option = "plasma") + 
+  theme_minimal()
+
+
+
+
+# -----------------------------------------------------------
+# estimate slope of distance from mine vs covariate value
+# take the average slope across years
+mine_impact_summary <- 
+  mine_impacts_df_cont |> 
+  dplyr::group_by(var, year) |> 
+  dplyr::group_modify(~{  # define a function that can iterate on the grouped tibble
+    
+    df <- .x
+    df <- filter(df, !is.na(value_scaled), !is.na(pixel_distance))
+    model <- lm(value_scaled ~ pixel_distance, data = df)
+    model_summary <- summary(model)
+    tibble(beta = coef(model_summary)["pixel_distance", "Estimate"],
+           p_value = coef(model_summary)["pixel_distance", "Pr(>|t|)"],
+           adj_r2  = model_summary$adj.r.squared)
+    
+  }) |> 
+  dplyr::ungroup() |> 
+  dplyr::group_by(var) |> 
+  dplyr::summarise(
+    beta_mean = mean(beta, na.rm = TRUE),
+    beta_sd = sd(beta, na.rm = TRUE),
+    p_mean = mean(p_value, na.rm = TRUE),
+    p_sd = sd(p_value, na.rm = TRUE),
+    adj_r2_mean = mean(adj_r2, na.rm = TRUE),
+    adj_r2_sd = sd(adj_r2, na.rm =TRUE),
+    .groups = "drop"
+  )
+
+saveRDS(mine_impact_summary, file="C:/Users/mannf/Downloads/mine_impact_summary.rds")
 
 
