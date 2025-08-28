9fd943f99e413aeb46b155e761842c0ceb665537
mannfred boehm <mannfred.boehm@ubc.ca>
2025-08-27 20:51:27 -0700

    stacking

--- Rscripts/may_restart/06_train_models.R
+++ Rscripts/may_restart/06_train_models.R
@@ -4,13 +4,24 @@
 # created: May 7, 2025
 # ---
 
-
+library(BAMexploreR)
 library(tidyverse)
 library(terra)
 
 # set root path
 root <- "G:/Shared drives/BAM_NationalModels5"
 bam_boundary <- terra::vect(file.path(root, "Regions", "BAM_BCR_NationalModel_UnBuffered.shp"))
+
+# store predictor metadata as a reference
+predictor_metadata <- 
+  dplyr::tibble(BAMexploreR::predictor_metadata) |>
+  dplyr::filter(version == "v5") |> 
+  dplyr::select(predictor, definition, predictor_class) |> 
+  dplyr::mutate(across('predictor', str_replace, 'Year', 'year')) |> 
+  dplyr::mutate(across('predictor', str_replace, 'Method','method'))
+  
+categorical_predictors <- c("ABoVE_1km", "method", "NLCD_1km","MODISLCC_1km", 
+                            "MODISLCC_5x5", "SCANFI_1km", "VLCE_1km")
 
 
 # for every year (1990-2020)
@@ -31,10 +42,11 @@ hb_filename = "hydrobasins_masked.gpkg",
 soils_filename = "isric_soil_covariates_masked.tif"
 
 
+years <- seq(from = 1990, to = 2020, by = 5)
 
 
-
-backfill_mines_cont <- function(year, root){
+backfill_mines_cont <- 
+  function(year, root, categorical_predictors){
   
   
   ### 
@@ -70,7 +82,7 @@ backfill_mines_cont <- function(year, root){
   
      mine_centroid <- terra::centroids(mine_poly_y)
      
-     # identify corresponding sub-basin, or nearest sub
+     # identify corresponding sub-basin
      hit <- which(terra::relate(mine_centroid, subbasins, relation = "within"))
      
      return(subbasins[hit])
@@ -81,6 +93,7 @@ backfill_mines_cont <- function(year, root){
   # identify subbasin for each mine patch
   # seq_len creates a vector of integers {1, 2,...N}, where N is the number of mine polygons,
   # i is the index where the above integers iterate through
+  print("assigning sub-basins to mines")
   subbasins_for_mines <- lapply(seq_len(nrow(mines_polygons_y)), function(i) {
     subbasin_search(mines_polygons_y[i, ])
   })
@@ -99,6 +112,7 @@ backfill_mines_cont <- function(year, root){
   
   # summary stats
   subbasin_stats <- tibble(
+    total_subbasins = length(areas_km2),
     total_area = sum(areas_km2),
     mean_area  = mean(areas_km2),
     sd_area    = sd(areas_km2),
@@ -109,14 +123,16 @@ backfill_mines_cont <- function(year, root){
   
 
   ### 
-  # import V5 covariate stack (BCR x year), year-matched disturbance layer, soil layer
+  # find BCR(s) that intersect with subbasin_s and import V5 covariate stacks,
+  # year-matched disturbance layer, and soil layer
   ###
  
   # get BCR(s) per subbasin
-  # many subbasins overlap with multiple BCR ids, so
-  # we give each unique combination of BCRs returned by bam_get_bcr an identifier
+  # some subbasins overlap with multiple BCR ids, so
+  # we give each unique combination of BCRs an identifier
   # this way we can stack and train models once per unique BCR combo 
   # (instead of repeating stacking/training for each subbasin)
+  print("identifying BCR-subbasin intersections")
   bcr_list <- lapply(seq_len(nrow(all_subbasins)), function(i) {
         BAMexploreR::bam_get_bcr(version = "v5", ext = all_subbasins[i])
     })
@@ -125,40 +141,154 @@ backfill_mines_cont <- function(year, root){
   # (53 unique multi- or single bcr combinations)
   bcr_keys <- vapply(bcr_list, function(v) {
     if (length(v) == 0) "" else paste(v, collapse = "_")
-  }, character(1))
+    }, character(1))
   
-  # associate a bcr key info (535) to each mine-subbasin (535)
+  # associate bcr key info (535) to each mine-subbasin (535)
   all_subbasins$bcr_keys <- bcr_keys
   
   # identify which mine-subbasins belong to which bcr group
+  # for each group we'll train a separate model 
+  # there are 52 groups vs 535 subbasins, so we save time by not 
+  # redundantly training models on the same BCRs/BCR groups 
   bcr_groups <- split(seq_len(nrow(all_subbasins)), all_subbasins$bcr_keys)
   
-  # create a new environment to 
-  # cache mosaicked stacks per combo_key
-  stack_cache <- new.env(parent = emptyenv())
+  # build one dissolved polygon per unique BCR key
+  # returns a named list: combo_polys[["can40_can41"]] = SpatVector (1 row)
+  combo_polys <- lapply(names(bcr_groups), function(key) {
+    idx <- bcr_groups[[key]]
+    poly <- terra::aggregate(all_subbasins[idx, ], "bcr_keys")
+    # dissolve keeps the attribute column; filter to the single row for this key
+    poly[poly$bcr_keys == key, ]
+  })
+  names(combo_polys) <- names(bcr_groups)
   
-  # fetch the appropriate V5 covariate stack for year_y
-  get_stack_for_key <- function(key, year) {
-    cache_id <- paste(key, year, sep = "_")
-    if (exists(cache_id, envir = stack_cache)) return(get(cache_id, envir = stack_cache))
+  
+  # fetch the appropriate V5 covariate stack(s) for year_y
+  # mosaic stacks if the subbasin intersects multiple BCRs
+  create_stack_for_subbasin <- function(key, year) {
     
-    # parse "can10_can40" -> c("can10", "can40")
+    cache_id <- paste(key, year, sep = "_")
+    
+    if (exists(cache_id, envir = stack_cache)) {
+      return(get(cache_id, envir = stack_cache))
+    }
+    
+    combo_poly <- combo_polys[[key]]
+    
+    # convert "can10_can40" to c("can10.tif", "can40.tif")
     bcr_keys_split <- paste(strsplit(key, "_", fixed = TRUE)[[1]], paste0(year, ".tif"), sep="_")
     
-    # load each BCR × year stack, then mosaic
-    stacks <- lapply(bcr_keys_split, function(r) terra::rast(file.path(root, "gis", "stacks", r)))
+    # load each BCR × year stack and crop to relevant subbasins
+    print("loading covariate stacks")
+    stacks <- lapply(bcr_keys_split, function(r) {
+      
+      terra::rast(file.path(root, "gis", "stacks", r)) |> 
+      terra::crop(x = _, y = combo_poly) |> 
+      terra::mask(x = _, mask = combo_poly)
+      
+    })
+    # before mosaicking, apply `as.factor` to categorical layers
+    mark_categoricals <- function(stack_s, categorical_predictors) {
+      keep <- intersect(categorical_predictors, names(stack_s))
+      for (marked_layer in keep) stack_s[[marked_layer]] <- terra::as.factor(stack_s[[marked_layer]])
+      stack_s
+    }
+    
+    print("applying as.factor to categorical layers in covariate stacks")
+    stacks <- lapply(X = stacks, FUN = mark_categoricals, categorical_predictors = categorical_predictors)
+    
+    # single-layer template grid for this BCR-group
+    grid_tmpl <- terra::rast(
+      extent = terra::align(ext(combo_poly), stacks[[1]][[1]], snap = "out"),
+      resolution = terra::res(stacks[[1]]),
+      crs = terra::crs(stacks[[1]])
+    ) 
+    
+
+    
+    # mosaic categorical layers across BCR stacks
+    # for a given categorical predictor:
+    # 1) identify which stacks it exists in
+    # 2) identify the layers it exists in
+    # 3) mosaic those layers
     
     
-    # MOSAICNIG?????
-    stacks_mos <- lapply(stacks, function(s) terra::mosaic(s))
     
-    assign(cache_id, stk, envir = stack_cache)
-    stk
-  }
+    # in the case of a sub-basin intersecting multiple BCRs
+    # mosaic BCR x year stacks together to create a single training space
+    print("mosaic-ing covariate layers for sub-basins that intersect multiple BCRs")
+    if (length(stacks) == 1) {
+      
+      stacks_mos <- stacks[[1]]
+      
+    } else {
+      
+      # extract all covariate names from all stacks
+      all_names <- unique(unlist(lapply(stacks, names)))
+      
+      # mosaic by layer
+      mosaic_layers <- lapply(all_names, function(name) {
+        
+        # collect layer_l from each stack where present
+        rlist <- lapply(stacks, function(stack_s) {
+          if (name %in% names(stack_s)) stack_s[[name]] else NULL
+        })
+        
+        # remove NULLs if they exist
+        rlist <- Filter(Negate(is.null), rlist)
+        
+        # if only one stacks has this layer, return it as-is
+        # still, align result to the common grid_tmpl
+        if (length(rlist) == 1) {
+          out <- rlist[[1]]
+          if (name %in% categorical_predictors) {
+            out <- terra::resample(out, grid_tmpl, method = "near")
+          } else {
+            out <- terra::resample(out, grid_tmpl, method = "bilinear")
+          }
+          return(out)
+        }
+        
+        
+        # align all contributors to the BCR-group grid
+        if (name %in% categorical_predictors) {
+          rlist <- lapply(rlist, function(r)
+            if (!terra::compareGeom(r, grid_tmpl, stopOnError = FALSE))
+              terra::resample(r, grid_tmpl, method = "near") else r)
+        } else {
+          rlist <- lapply(rlist, function(r)
+            if (!terra::compareGeom(r, grid_tmpl, stopOnError = FALSE))
+              terra::resample(r, grid_tmpl, method = "bilinear") else r)
+        }
+        
+        
+        # stack contributors for this layer (extents should already match after crop/mask)
+        layer_stack <- do.call(c, rlist)
+        
+        # mosaic layers shared between stacks
+        if (name %in% categorical_predictors) {
+        
+          out <- terra::app(layer_stack, fun = function(x) terra::modal(x, ties = "first", na.rm = TRUE))
+         
+          
+         } else {
+            
+          out <- terra::app(layer_stack, fun = mean, na.rm = TRUE)
+      
+         }
+        
+        out 
+        
+        }) # finish splitting and mosaicing multi-BCR groups
+      
+      # combine mosaics into a single stack 
+      stacks_mos <- do.call(c, mosaic_layers)
+      names(stacks_mos) <- all_names
+    } # finish mosaicking
   
-  
-     
+assign(cache_id, stacks_mos, envir = stack_cache)
 return(subbasin_stats)
+return(stacks_mos)
      
 } # close backfill_mines_cont()
 
