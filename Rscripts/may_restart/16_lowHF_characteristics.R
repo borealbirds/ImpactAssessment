cb3bc42347af0b52bff27695da1bcf86609f03f5
Mannfred Masahiro Asada Boehm <mannfred.boehm@ubc.ca>
2025-12-09 21:49:44 -0800

    start multivariate analysis of low and high pixels

--- Rscripts/may_restart/16_lowHF_characteristics.R
+++ Rscripts/may_restart/16_lowHF_characteristics.R
@@ -4,6 +4,7 @@
 # created: December 9, 2025
 # ---
 
+library(MASS)
 library(terra)
 library(tidyterra)
 library(tidyverse)
@@ -52,7 +53,7 @@ ggplot() +
 
 
 # ---------------------------------------------------
-# analyse low HF traits in randomly selected subbasins
+# prepare dataframes from randomly selected subbasins
 
 # convert stacks from random subbasins to dataframe
 cov_df <- terra::as.data.frame(cov_s)
@@ -71,7 +72,37 @@ highhf_mask <- terra::rast(file.path(ia_dir, "hirshpearson", "CanHF_1km_morethan
 highhf_mask <- terra::project(x=highhf_mask, y=cov_s, method = "near")
 cov_highhf  <- terra::mask(cov_s, highhf_mask) 
 cov_highhf_df <- terra::as.data.frame(cov_highhf)
+saveRDS(cov_highhf_df, file=file.path(getwd(), "data/derived_data/rds_files/highhf_covariate_df_random_subbasins_2020.rds"))
 
-saveRDS(cov_highhf, file=file.path(getwd(), "data/derived_data/rds_files/highhf_covariate_df_random_subbasins_2020.rds"))
 
+# ---------------------------------------------------
+# test for differences in environmental features between datasets
 
+# combine low and high HF data
+df <- rbind(cov_highhf_df, cov_lowhf_df)
+group <- factor(c(rep("high", nrow(cov_highhf_df)),
+                  rep("low",  nrow(cov_lowhf_df))))
+
+# remove invariant features
+inv_covs <- sapply(df, function(x) length(unique(x)) == 1)
+df2 <- df[, !inv_covs] # 85 covariates remain
+
+# remove categorical features ("ABoVE_1km", "NLCD_1km" aren't in this subset)
+categorical_responses <- c("MODISLCC_1km", "MODISLCC_5x5","SCANFI_1km","VLCE_1km")
+df3 <- dplyr::select(df2, -all_of(categorical_responses))
+
+# remove disturbances because they will differ by definition
+disturbances <- c("CCNL_1km", "CanHF_1km", "CanHF_5x5", "canroad_1km", "canroad_5x5" )
+df4 <- dplyr::select(df3, -all_of(disturbances))
+
+group <- factor(c(rep("high", nrow(cov_highhf_df)),
+                  rep("low",  nrow(cov_lowhf_df))))
+
+df5 <- data.frame(group = group, df4)
+df5[,-1] <- lapply(df5[,-1], function(x) as.numeric(as.character(x)))
+
+# no complete cases :-()
+sum(complete.cases(df5))
+
+# fit LDA
+lda_fit <- lda(group ~ ., data = df5)
